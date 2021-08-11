03aac6ce01d114a449af97e66756e9971238b31b
Michel Henrique <mic-andrade@hotmail.com>
2021-08-11 11:03:37 -0300

    test: ensure Login calls validation with correct password

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -47,4 +47,12 @@ describe('Login Component', () => {
       email: 'any_email'
     })
   })
+  test('should call Validation with correct password', () => {
+    const { sut, validationSpy } = makeSut()
+    const passwordInput = sut.getByTestId('password')
+    fireEvent.input(passwordInput, { target: { value: 'any_password' } })
+    expect(validationSpy.input).toEqual({
+      password: 'any_password'
+    })
+  })
 })
