4a5da50321d20c4063c54a870598ee74b9c7178d
Michel Henrique <mic-andrade@hotmail.com>
2021-08-14 18:10:14 -0300

    test: ensure Login disable submit button only if form is invalid

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -69,4 +69,14 @@ describe('Login Component', () => {
     expect(emailStatus.title).toBe('Tudo certo')
     expect(emailStatus.textContent).toBe('🟢')
   })
+  test('should enable submit button if form is valid', () => {
+    const { sut, validationStub } = makeSut()
+    validationStub.errorMessage = null
+    const emailInput = sut.getByTestId('email')
+    fireEvent.input(emailInput, { target: { value: faker.internet.email() } })
+    const passwordInput = sut.getByTestId('password')
+    fireEvent.input(passwordInput, { target: { value: faker.internet.password() } })
+    const submitButton = sut.getByTestId('submit') as HTMLButtonElement
+    expect(submitButton.disabled).toBe(false)
+  })
 })
