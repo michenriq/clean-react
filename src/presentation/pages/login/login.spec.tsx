a8213e057fcf0f9d6532d8550f7305680821bcd4
Michel Henrique <mic-andrade@hotmail.com>
2021-08-14 19:12:35 -0300

    test: prevent Login to submit form if itsinvalid

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -103,10 +103,17 @@ describe('Login Component', () => {
       password
     })
   })
-  test('should call Authentication only oce', () => {
+  test('should call Authentication only once', () => {
     const { sut, authenticationSpy } = makeSut()
     simulateValidSubmit(sut)
     simulateValidSubmit(sut)
     expect(authenticationSpy.callsCount).toBe(1)
+  })
+  test('should not call Authentication if form is invalid', () => {
+    const validationError = faker.random.words()
+    const { sut, authenticationSpy } = makeSut({ validationError })
+    populateEmailField(sut)
+    fireEvent.submit(sut.getByTestId('form'))
+    expect(authenticationSpy.callsCount).toBe(0)
   })
 })
