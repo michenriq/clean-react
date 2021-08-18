e8673bac37073e635f0623eac84877928ee4da1a
Michel Henrique <mic-andrade@hotmail.com>
2021-08-18 07:47:52 -0300

    refactor: move factories to their own files

---
+++ src/main/factories/pages/login/login-factory.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Login } from '@/presentation/pages'
+import { makeRemoteAuthentication } from '@/main/usecases/authentication/remote-authentication-factory'
+import { makeLoginValidation } from './login-validation-factory'
+
+export const makeLogin: React.FC = () => {
+  return (
+    <Login authentication={makeRemoteAuthentication()} validation={makeLoginValidation()}/>
+  )
+}
