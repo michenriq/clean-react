3e65066fc82d9048fecbcede17f734ced5f458f5
Michel Henrique <mic-andrade@hotmail.com>
2021-08-22 13:10:02 -0300

    feat: ensure RemoteAddAccount throws EmailInUseError if httpPostClient return 403

--- src/data/usecases/add-account/remote-add-account.ts
+++ src/data/usecases/add-account/remote-add-account.ts
@@ -1,4 +1,5 @@
-import { HttpPostClient } from '@/data/protocols/http'
+import { HttpPostClient, HttpStatusCode } from '@/data/protocols/http'
+import { EmailInUseError } from '@/domain/errors'
 import { AccountModel } from '@/domain/models'
 import { AddAccount, AddAccountParams } from '@/domain/usecases'
 
@@ -13,6 +14,9 @@ export class RemoteAddAccount implements AddAccount {
       url: this.url,
       body: params
     })
-    return null
+    switch (httpResponse.statusCode) {
+      case HttpStatusCode.forbidden: throw new EmailInUseError()
+      default: return null
+    }
   }
 }
